677704cf55c090e60f15531733e669b42c68ca5d
sevenflow <648813710@qq.com>
2021-06-19 15:29:54 +0800

    feat:增加文件loader功能&加入eslint风格功能

--- src/index.js
+++ src/index.js
@@ -1,49 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import List from './components/list'
-import * as spritejs from "spritejs";
+import * as spritejs from 'spritejs'
 import styles from './index.less'
 
-export default  () => {
+import img1 from './../assets/img/1.png'
+import img3 from './../assets/img/3.png'
+import img2 from './../assets/img/2.png'
+import img4 from './../assets/img/4.png'
+import img5 from './../assets/img/5.png'
+import img6 from './../assets/img/6.png'
+
+export default () => {
   // 初始画布
   const initCanvas = () => {
-    const { Scene, Arc } = spritejs;
-    const container = document.getElementById("bottom");
+    const { Scene, Arc } = spritejs
+    const container = document.getElementById('bottom')
     const scene = new Scene({
       container,
       width: 1200,
       height: 600,
-    });
-    const layer = scene.layer();
-    
+    })
+    const layer = scene.layer()
+
     const fan = new Arc({
       pos: [300, 300],
       radius: 100,
       startAngle: 0,
       endAngle: 120,
       fillColor: 'red',
-    });
-    layer.append(fan);
-    
-    const fan2 = fan.cloneNode();
+    })
+    layer.append(fan)
+
+    const fan2 = fan.cloneNode()
     fan2.attr({
       pos: [300, 150],
       closeType: 'sector',
-    });
-    
-    layer.append(fan2);
-  };
+    })
+
+    layer.append(fan2)
+  }
 
 
   useEffect(() => {
-  console.log(GLOBAL_ENV)
-  initCanvas()
-  }, []);
+    console.log(GLOBAL_ENV)
 
-return (
+    initCanvas()
+  }, [])
+
+  return (
     <div className={styles.App}>
-      <List data={[1,2,3]}/>
+      <List data={[img1, img2, img3, img4, img5, img6]} />
       <div className={styles.bottom} id="bottom">
-        <div className={styles.bottomTop}></div>
+        <div className={styles.bottomTop} />
       </div>
     </div>)
 }
